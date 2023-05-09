87e56076af465cf69c07df92da303c251fe57481
alberto <alberto@simulador-TCP.myguest.virtualbox.org>
2023-05-09 22:54:03 +0200

    feat: First event-driven tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,43 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import { App } from "./App";
+import userEvent from "@testing-library/user-event";
 
 describe("test the App root component", () => {
-    it("should render the awesome text", () => {
+    it("should correctly display the created item on the screen", async () => {
+        const user = userEvent.setup();
         render(<App/>);
-        expect(screen.getByText(/awesome/)).toBeInTheDocument();
+
+        const itemDescription = screen.getByRole("textbox");
+        const createButton = screen.getByRole("button");
+
+        await user.click(itemDescription);
+        await user.keyboard("First item");
+        await user.click(createButton);
+
+        expect(itemDescription).toHaveValue(""); 
+        expect(screen.getByText("First item")).toBeInTheDocument();
+
+        await user.click(itemDescription);
+        await user.keyboard("Second item");
+        await user.click(createButton);
+
+        expect(itemDescription).toHaveValue(""); 
+        expect(screen.getByText("Second item")).toBeInTheDocument();
+
+        await user.click(createButton);
+        expect(screen.getAllByRole("listitem")).toHaveLength(2);
+    });
+
+    it("should not add empty items to the list", async () => {
+        const user = userEvent.setup();
+        render(<App/>);
+
+        const itemDescription = screen.getByRole("textbox");
+        const createButton = screen.getByRole("button");
+
+        await user.click(createButton); //try to create an item with empty description
+        expect(screen.getAllByRole("listitem")).toHaveLength(0);
     });
 });
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,33 @@
+import React, { useState } from "react";
+import {Button, Input} from "@mui/material";
+
 export const App = () => {
+    
+    const [itemDescription, setItemDescription] = useState<string>("");
+    const [items, setItems] = useState<string[]>([]);
+
     return (
-        <div>This is an awesome new react app</div>
+        <div>
+            <Input
+             value={itemDescription}
+             onChange={(event) => {
+                event.preventDefault(); //
+                setItemDescription(event.target.value);
+             }}
+            />
+            <Button
+                onClick={()=>{
+                    if (itemDescription !== "") {
+                        setItems([...items, itemDescription]);
+                    }
+                    setItemDescription("");
+                }}
+            >
+                Create
+            </Button>
+            <ul>
+                {items.map((item) => <li>{item}</li>)}
+            </ul>
+        </div>
     );
 };
